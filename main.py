c3edc57b9c5e382076757debc205c30062050466
mmirgova51021 <m.mirgova@gmail.com>
2025-04-04 16:33:30 +0200

    main.py

--- main.py
+++ main.py
@@ -4,7 +4,6 @@ import os
 
 app = Flask(__name__)
 
-# Čítanie z environment variables
 BOT_TOKEN = os.getenv('BOT_TOKEN')
 CHAT_ID = os.getenv('CHAT_ID')
 
@@ -15,16 +14,17 @@ def send_telegram_message(message):
         "text": message
     }
     response = requests.post(url, json=payload)
-    print("Telegram odpoveď:", response.status_code, response.text)  # DEBUG výpis
     return response.json()
 
-@app.route('/', methods=['POST'])
+@app.route("/", methods=["POST"])
 def receive_alert():
     data = request.get_json()
-    message = data.get('message', 'RSI otočka signál bez správy.')
+    message = data.get("message", "RSI otočka signál bez správy.")
     send_telegram_message(message)
-    return {'status': 'ok'}, 200
+    return {"status": "ok"}, 200
 
 # Spustenie servera
-app.run(host='0.0.0.0', port=8080)
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=8080)
+
 
